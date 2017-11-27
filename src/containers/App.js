d4c1fa5d4f70362f503699d87aceb3be36483425
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-11-27 20:51:59 +0000

    add bootstrap to give initial style

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,8 +6,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi9l4SJzd_XAhXGJcAKHeyQDCwQjRwIBw&url=https%3A%2F%2Fnorthcoders.com%2F&psig=AOvVaw1rZf4_NHw_NZG2Ri3QXvqk&ust=1511901197014492" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="/logo_white.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">News</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
