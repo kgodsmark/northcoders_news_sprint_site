3b69acaa4f688eace9db350735f28bd1cfcce0f6
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-11-27 20:40:02 +0000

    react app launches

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi9l4SJzd_XAhXGJcAKHeyQDCwQjRwIBw&url=https%3A%2F%2Fnorthcoders.com%2F&psig=AOvVaw1rZf4_NHw_NZG2Ri3QXvqk&ust=1511901197014492" className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './containers/App';
+
+
+ReactDOM.render(
+    <Provider store={store} >
+        <Router>
+            <Switch>
+                <Route exact path='/' component={App} />
+            </Switch>
+        </Router>
+    </Provider>, document.getElementById('root'));
