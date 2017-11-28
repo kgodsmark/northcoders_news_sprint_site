f572c8cb6d140c4246b8bd79006b2b18dc313bff
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-11-28 16:35:03 +0000

    create redux action for fetchArticle

---
+++ src/actions/fetchArticle.js
@@ -0,0 +1,32 @@
+import * as types from './types';
+import axios from 'axios';
+
+import API_URL from '../api_url';
+
+
+export const fetchArticleRequest = () => ({
+  type: types.FETCH_ARTICLE_REQUEST
+});
+
+export const fetchArticleSuccess = (data) => ({
+  type: types.FETCH_ARTICLE_SUCCESS,
+  payload: data
+});
+
+export const fetchArticleFailure = (error) => ({
+  type: types.FETCH_ARTICLE_FAILURE,
+  payload: error
+});
+
+export default (article_id) => {
+  return (dispatch) => {
+    dispatch(fetchArticleRequest());
+    return axios.get(`${API_URL}/articles/${article_id}`)
+      .then(res => {
+        dispatch(fetchArticleSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchArticleFailure(err.response.data));
+      });
+  }
+};
