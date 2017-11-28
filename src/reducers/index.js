7ff335872931e3e1e780d248bd0c66f943de504f
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-11-28 21:28:40 +0000

    create redux reducer for comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-
+import articles from './articles';
+import article from './article';
+import comments from './comments';
 
 const reducer = combineReducers({
-
+    articles, article, comments
 });
 
 export default reducer;
