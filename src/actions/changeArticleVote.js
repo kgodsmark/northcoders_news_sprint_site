d08f23d8074bd17db605a5f0212e7bdbf1d2643c
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-01 23:29:02 +0000

    create redux action for changeArticleVote

---
+++ src/actions/changeArticleVote.js
@@ -0,0 +1,31 @@
+import * as types from './types';
+import axios from 'axios';
+
+import API_URL from '../api_url';
+
+  export const changeArticleVoteRequest = () => ({
+    type: types.PUT_ARTICLE_REQUEST,
+  });
+    
+  export const changeArticleVoteSuccess = (article) => ({
+    type: types.PUT_ARTICLE_SUCCESS,
+    payload: article
+  });
+    
+  export const changeArticleVoteFailure = (error) => ({
+    type: types.PUT_ARTICLE_FAILURE,
+    payload: error
+  });
+  
+  export default (article_id, voteChange) => {
+    return (dispatch) => {
+      dispatch(changeArticleVoteRequest());
+      return axios.patch(`${API_URL}/articles/${article_id}?vote=${voteChange}`)
+        .then(res => {
+          return dispatch(changeArticleVoteSuccess(res.data.article));
+        })
+        .catch(err => { 
+          return dispatch(changeArticleVoteFailure(err.response.data));
+        });
+    };
+  };
