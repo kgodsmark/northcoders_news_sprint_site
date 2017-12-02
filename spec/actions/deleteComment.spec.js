2485f95fff84cd779d69df98bb3d4c55e37a8625
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-02 15:42:34 +0000

    tests passing for deleteComment action

---
+++ spec/actions/deleteComment.spec.js
@@ -0,0 +1,65 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import moxios from 'moxios';
+import expect from 'expect';
+import deleteComment, {
+  deleteCommentRequest,
+  deleteCommentSuccess,
+  deleteCommentFailure
+} from '../../src/actions/deleteComment';
+
+import API_URL from '../../src/api_url';
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('deleteComment actions', () => {
+
+  beforeEach(function () {
+    moxios.install();
+  });
+
+  afterEach(function () {
+    moxios.uninstall();
+  });
+
+  it('dispatches DELETE_COMMENT_SUCCESS, responding with 200 and data', () => {
+    moxios.stubRequest(`${API_URL}/comments/12345`, {
+      method: 'delete',
+      status: 200,
+      response: {comments:[1, 2, 3]},
+    });
+
+    const expectedActions = [
+      deleteCommentRequest(),
+      deleteCommentSuccess([1, 2, 3])
+    ];
+
+    const store = mockStore()
+
+    return store.dispatch(deleteComment('12345')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+  
+  it('dispatches DELETE_COMMENT_FAILURE, responding with an error', () => {
+    const error = new Error('Error: Request failed with status code 400');
+    
+    moxios.stubRequest(`${API_URL}/comments/none`, {
+       status: 400,
+       response: { error }
+    });
+
+    const expectedActions = [
+      deleteCommentRequest(),
+      deleteCommentFailure({ 'error': error})
+    ];
+
+    const store = mockStore();
+
+    return store.dispatch(deleteComment('none'))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+  });
+});
--- src/actions/deleteComment.js
+++ src/actions/deleteComment.js
@@ -3,29 +3,29 @@ import axios from 'axios';
 
 import API_URL from '../api_url';
 
-  export const postCommentRequest = () => ({
+  export const deleteCommentRequest = () => ({
     type: types.DELETE_COMMENT_REQUEST,
   });
     
-  export const postCommentSuccess = (comments) => ({
+  export const deleteCommentSuccess = (comments) => ({
     type: types.DELETE_COMMENT_SUCCESS,
     payload: comments
   });
     
-  export const postCommentFailure = (error) => ({
+  export const deleteCommentFailure = (error) => ({
     type: types.DELETE_COMMENT_FAILURE,
     payload: error
   });
   
   export default (comment_id) => {
     return (dispatch) => {
-      dispatch(postCommentRequest());
-      return axios.post(`${API_URL}/comments/${comment_id}`)
+      dispatch(deleteCommentRequest());
+      return axios.delete(`${API_URL}/comments/${comment_id}`)
         .then(res => {
-          return dispatch(postCommentSuccess(res.data.comments));
+          return dispatch(deleteCommentSuccess(res.data.comments));
         })
         .catch(err => { 
-          return dispatch(postCommentFailure(err.response.data));
+          return dispatch(deleteCommentFailure(err.response.data));
         });
     };
   };
