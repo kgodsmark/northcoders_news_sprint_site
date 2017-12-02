0e7b2923d4caf229abf9bb57c235fe883efcf968
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-02 15:59:16 +0000

    create redux action to fetch User

---
+++ src/actions/fetchUser.js
@@ -0,0 +1,32 @@
+import * as types from './types';
+import axios from 'axios';
+
+import API_URL from '../api_url';
+
+
+export const fetchUserRequest = () => ({
+  type: types.FETCH_USER_REQUEST
+});
+
+export const fetchUserSuccess = (data) => ({
+  type: types.FETCH_USER_SUCCESS,
+  payload: data
+});
+
+export const fetchUserFailure = (error) => ({
+  type: types.FETCH_USER_FAILURE,
+  payload: error
+});
+
+export default (username) => {
+  return (dispatch) => {
+    dispatch(fetchUserRequest());
+    return axios.get(`${API_URL}/users/${username}`)
+      .then(res => {
+        dispatch(fetchUserSuccess(res.data.user));
+      })
+      .catch(err => {
+        dispatch(fetchUserFailure(err.response.data));
+      });
+  }
+};
