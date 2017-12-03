d58d2e9bd2ab83463b99b17380c5c60ed6e4be60
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-03 19:49:33 +0000

    implement article view style on buttons

--- src/components/ArticleViewUI.js
+++ src/components/ArticleViewUI.js
@@ -4,19 +4,35 @@ import CommentsList from '../containers/CommentsList';
 
 const ArticleViewUI = ({ loading, article, onNextPage, onPrevPage, nextPage, onVoteUp, onVoteDown, topic, nextId, prevId }) => (
     <div>
-        <Link to={`/articles/${nextId}?topic=${topic}`}><button onClick={onNextPage}>Next Article</button></Link>
-        <Link to={`/articles/${prevId}?topic=${topic}`}><button onClick={onPrevPage}>Previous Article</button></Link>
-        {(loading) ? <h3>Loading...</h3> : article.map((article, i) => (
-            <div>
-                <h3 key={`title${i}`}>{article.title}</h3>
-            <p key={`body${i}`}>{article.body}</p>
-            <Link to={`/users/${article.created_by}`}><h5 key={`created${i}`}>Author: {article.created_by}</h5></Link>
-            <p key={`votes${i}`}>Votes:{article.votes}</p>
-            <button onClick={onVoteUp}>Vote up</button>
-            <button onClick={onVoteDown}>Vote down</button>
-            <CommentsList/>
+
+
+        <div className="card bg-light mb-3" style={{ maxWidth: "spacing + 'em'" }} >
+            <div className="card-header">
+                <Link to={`/articles/${prevId}?topic=${topic}`}><button onClick={onPrevPage} className="actionButton prevArrow"><img src="/arrow-left.svg" width="20px" alt="arrow-left" /> Previous Article</button></Link>
+                <Link to={`/articles/${nextId}?topic=${topic}`}><button onClick={onNextPage} className="actionButton nextArrow">Next Article <img src="/arrow-right.svg" width="20px" alt="arrow-right" /></button></Link>
+
+                <span class="oi oi-icon-name" title="icon name" aria-hidden="true"></span>
             </div>
-        ))}
+
+            {(loading) ? <h3>Loading...</h3> : article.map((article, i) => (
+                <div>
+                    <div className="card-body">
+                        <h4 className="card-title" key={`title${i}`}>{article.title}</h4>
+                        <p className="card-text" key={`body${i}`}>{article.body}</p>
+                        <h5 key={`created${i}`}>By: <Link to={`/users/${article.created_by}`}>{article.created_by}</Link></h5>
+                        <div>
+                        <span key={`votes${i}`}>Vote <button className="actionButton" onClick={onVoteUp}><img src="/arrow-thick-top.svg" height="20px" alt="arrow-thick-top" /> </button>{article.votes}</span>
+                        <button  className="actionButton" onClick={onVoteDown}><img src="/arrow-thick-bottom.svg" height="20px" alt="arrow-thick-bottom" /> </button>
+                    </div>
+                    </div>
+
+
+                    <div>
+                    <CommentsList />
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
 );
 
