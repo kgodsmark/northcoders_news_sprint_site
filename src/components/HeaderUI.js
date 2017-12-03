0498efbaf9adc18e9d4b16c40108f81f0e2783be
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-03 19:50:59 +0000

    implement style on the header, adding the news img

--- src/components/HeaderUI.js
+++ src/components/HeaderUI.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HeaderUI = () => (
     <div>
         <header className="App-header">
-          <img src="/logo_white.png" className="App-logo" alt="logo" />
+          <div className="App-top-logo"><img src="/logo_white.png" className="App-logo" alt="logo" /><img src="/news.png" className="App-top-news-logo" alt="logo" /></div>
           <h1 className="App-title">Vote for your favourite articles and tell us what you think</h1>
         </header>
     </div>
