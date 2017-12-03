671babb6061bd41a7cc8a9eb31a15b78bbc065eb
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-03 19:50:34 +0000

    implement style on article view page now showing only top 15 articles

--- src/components/ArticleListUI.js
+++ src/components/ArticleListUI.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
 
 const ArticleListUI = ({ articles, loading }) => (
     <div id='ArticleList'>
-        {(loading) ? <h3>Loading...</h3> : articles.map(article => (
-            <div>
-            <h3 key={article._id}><Link to={`/articles/${article._id}?topic=${article.belongs_to}`}>{article.title}</Link></h3>
-            <span>Votes: {article.votes}</span>
-            </div>
-        ))}
+        <ListGroup>
+            {(loading) ? <h3>Loading...</h3> : articles.map(article => (
+                <div>
+                    <ListGroupItem className="justify-content-between"><Link to={`/articles/${article._id}?topic=${article.belongs_to}`}>{article.title}</Link>
+                    <div><span>By {article.created_by}  </span><Badge pill>{article.votes} votes</Badge></div></ListGroupItem>
+                    <h3 key={article._id}></h3>
+                </div>
+            ))}
+        </ListGroup>
     </div>
 );
 
