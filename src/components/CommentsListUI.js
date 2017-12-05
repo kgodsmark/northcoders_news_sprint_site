e1939a1c6312c658fa0ac4022d12e40162a72d6d
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-05 14:01:23 +0000

    formatting on Comments components

--- src/components/CommentsListUI.js
+++ src/components/CommentsListUI.js
@@ -3,39 +3,36 @@ import CommentFormUI from './CommentFormUI';
 import CommentUI from './CommentUI';
 
 const CommentsListUI = ({ comments, onSubmit, onNameChange, username, newComment, onTextChange, onVoteUp, onVoteDown, deleteComment }) => (
-  <div>
-
-    <div className="detailBox">
-      <div className="titleBox">
-        <label>Comments</label>
-      </div>
-      <div className="actionBox">
-        <ul className="commentList list-group">
-          {
-            comments.map((comment, i) => {
-              return (
-                <CommentUI 
+  <div className="detailBox">
+    <div className="titleBox">
+      <label>Comments</label>
+    </div>
+    <div className="actionBox">
+      <ul className="commentList list-group">
+        {
+          comments.map((comment, i) => {
+            return (
+              <CommentUI
                 comment={comment}
                 i={i}
                 onVoteUp={onVoteUp}
                 onVoteDown={onVoteDown}
                 deleteComment={deleteComment}
-                />
-              );
-            })
-          }
-        </ul>
+              />
+            );
+          })
+        }
+      </ul>
 
-        <CommentFormUI 
+      <CommentFormUI
         onSubmit={onSubmit}
         onNameChange={onNameChange}
         username={username}
         newComment={newComment}
         onTextChange={onTextChange}
-        />
-      </div>
+      />
     </div>
-  </div >
+  </div>
 );
 
 export default CommentsListUI;
